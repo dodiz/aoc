440e4246c9f2bf096d3dae04fb6644aaca6419bb
dodiz <dodiz>
2022-12-09 15:16:31 +0100

    1

---
+++ 2022/1/index.ts
@@ -0,0 +1,14 @@
+import { calories, caloriesTest } from "./input";
+
+const caloriesByElves = calories
+  .split("\n\n")
+  .map((arr) => arr.split("\n").reduce((acc, curr) => acc + Number(curr), 0));
+
+let totalCalories = 0;
+for (let i = 0; i < 3; i++) {
+  const maxCalories = Math.max(...caloriesByElves);
+  const index = caloriesByElves.findIndex((c) => c === maxCalories);
+  caloriesByElves.splice(index, 1);
+  totalCalories += maxCalories;
+}
+console.log(totalCalories);
