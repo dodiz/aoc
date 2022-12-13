8e1677858b61478b2c756e1f4e1f41152163953e
dodiz <dodiz>
2022-12-13 13:05:09 +0100

    12-13 wip

---
+++ 2022/13/index.ts
@@ -0,0 +1,69 @@
+import { inputMock, input } from "./input";
+type Packet = number[] & Packet[];
+function destructurePacket(packet: string) {
+  const arr: Packet = [];
+  const packetWithoutSquares = packet.slice(1, packet.length - 1);
+
+  for (let i = 0; i < packetWithoutSquares.length; i++) {
+    if (packetWithoutSquares.charAt(i) === ",") continue;
+    if (packetWithoutSquares.charAt(i) === "[") {
+      const subStartIndex = i;
+      let parenthesis = 1;
+      while (parenthesis > 0) {
+        i++;
+        if (packetWithoutSquares.charAt(i) === "]") parenthesis--;
+        else if (packetWithoutSquares.charAt(i) === "[") parenthesis++;
+      }
+      const subArr: Packet = destructurePacket(
+        packetWithoutSquares.slice(subStartIndex, i + 1)
+      );
+      arr.push(subArr);
+    }
+    let n = "";
+    for (
+      ;
+      packetWithoutSquares.charAt(i) !== "," &&
+      i < packetWithoutSquares.length &&
+      packetWithoutSquares.charAt(i) !== "]";
+      i++
+    ) {
+      n += packetWithoutSquares.charAt(i);
+    }
+    if (n) arr.push(+n);
+  }
+  return arr;
+}
+
+const pairs = inputMock.split("\n\n").map((pair) => {
+  const [firstPair, secondPair] = pair.split("\n");
+  return [destructurePacket(firstPair), destructurePacket(secondPair)];
+});
+
+let wrongOrderPairs = 0;
+function compareOrder(left: Packet, right: Packet): any {
+  console.log("comparing", left, right);
+  const max = left.length > right.length ? left.length : right.length;
+  for (let i = 0; i < max; i++) {
+    if (left[i] !== undefined && right[i] === undefined) return false;
+    if (left[i] === undefined && right[i] !== undefined) return true;
+    if (!Array.isArray(left[i]) && !Array.isArray(right[i])) {
+      if (left[i] < right[i]) return false;
+      if (left[i] > right[i]) return true;
+      else continue;
+    }
+    if (!Array.isArray(left[i]) && Array.isArray(right[i])) {
+      return compareOrder([left[i]], right[i]);
+    }
+    if (Array.isArray(left[i]) && !Array.isArray(right[i])) {
+      return compareOrder(left[i], [right[i]]);
+    }
+    return compareOrder(left[i], right[i]);
+  }
+}
+
+pairs.forEach(([first, second], i) => {
+  if (compareOrder(first, second)) {
+    console.log(`Pairs ${i + 1} in order`);
+  }
+});
+console.log(wrongOrderPairs);
